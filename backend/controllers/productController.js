a9115c6eaa009e443c16591ff65a8567ba3ce895
mhmd-sjad-nfsi <mhmdsjadnfysy@gmail.com>
2025-04-29 12:38:14 +0330

    size problem6

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,28 @@
 import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
-// … کنترلرهای getProducts, getProductById قبلاً تعریف شده‌اند
+// @desc    گرفتن تمام محصولات
+// @route   GET /api/products
+// @access  Public
+export const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+// @desc    گرفتن یک محصول با آیدی
+// @route   GET /api/products/:id
+// @access  Public
+export const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('محصول پیدا نشد');
+  }
+});
+
 
 // @desc    ایجاد محصول جدید
 // @route   POST /api/products
